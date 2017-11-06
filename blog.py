9dc7b923e3bb9ce29eefc0efa33bfb638cf6560d
sunyakun <sunyakun2016@outlook.com>
2017-11-06 16:44:07 +0800

    增加了一些接口

--- blog.py
+++ blog.py
@@ -16,6 +16,12 @@ def authentication(instance,request, decorated):
         resp.body = b'{"success":"false","msg":"authentication fail!"}'
         return resp
 
+def wrap_article_result(db_result):
+    if hasattr(db_result, '__getitem__'):
+        for i in db_result:
+            with open(UPLOAD+i['file'],'r') as file:
+                i['content'] = file.read()
+
 @bobo.query('/interface/login')
 def login(bobo_request,username,password):
     if username=='admin' and password=='123456':
@@ -35,12 +41,52 @@ def static(static):
 @bobo.query('/interface/get_article')
 def get_article(title=None,cls=None):
     query = tinydb.Query()
-    result = db.search((query.title==title) & (query.cls==cls))
-    for i in result:
-        with open(UPLOAD+i['file']) as file:
-            i['content'] = file.read()
+    result = []
+    if cls and title:
+        result = db.search((query.title==title) & (query.cls==cls))
+    else:
+        result = db.search((query.title==title) | (query.cls==cls))
+
+    wrap_article_result(result)
     resp = {"success":"true","result":result}
     return str(resp)
+
+@bobo.query('/interface/get_recent_article')
+def get_recent_article(limit=100):
+    result = db.all()[:int(limit)]
+    result.sort(key=lambda k:k['date'],reverse=True)
+    wrap_article_result(result)
+    return str({"success":"true","result":result})
+
+@bobo.query('/interface/create_cls', check=authentication)
+def create_cls(cls):
+    '''
+    meta table only has one json object like {"key":"meta","cls":["note"]}
+    '''
+    meta = db.table(name='meta')
+    if len(meta.all()) == 0:
+        meta.insert({"key":"meta","cls":[]})
+    origin = meta.all()[0]
+    if cls not in origin['cls']:
+        origin['cls'].append(cls)
+        meta.update(origin,tinydb.Query().key=="meta")
+        return '{"success":"true"}'
+    else:
+        return '{"success":"false","msg":"this class has already exist"}'
+
+@bobo.query('/interface/del_cls', check=authentication)
+def del_cls(cls):
+    '''
+    delete a given class
+    '''
+    meta = db.table(name='meta')
+    origin = meta.all()
+    if len(origin) == 0 or cls not in origin[0]['cls']:
+        return '{"success":"false","msg":"class not exist"}'
+    else:
+        origin[0]['cls'].remove(cls)
+        meta.update(origin[0],tinydb.Query().key=='meta')
+        return '{"success":"true"}'
 
 @bobo.query('/interface/publish_article',check=authentication)
 def pulish_article(file=None,title=None,desc=None,cls=None):
@@ -49,17 +95,22 @@ def pulish_article(file=None,title=None,desc=None,cls=None):
         with open(UPLOAD + file.filename,'wb') as new_file:
             new_file.write(file.file.read())
 
-    date = {
-        "year":datetime.datetime.now().year,
-        "month":datetime.datetime.now().month,
-        "day":datetime.datetime.now().day,
-        "hour":datetime.datetime.now().hour,
-        "minute":datetime.datetime.now().minute,
-        "second":datetime.datetime.now().second
-    }
+    # date = {
+    #     "year":datetime.datetime.now().year,
+    #     "month":datetime.datetime.now().month,
+    #     "day":datetime.datetime.now().day,
+    #     "hour":datetime.datetime.now().hour,
+    #     "minute":datetime.datetime.now().minute,
+    #     "second":datetime.datetime.now().second
+    # }
+    date =  datetime.datetime.now().isoformat()
+
     query = tinydb.Query()
     if hasattr(file, 'filename'):
         result = db.search((query.title==title) | (query.file==file.filename))
+        classes = db.table(name='meta').all()
+        if len(classes) == 0 or cls not in classes[0]['cls']:
+            return '{"succcess":"false","msg":"the class not exist,please create a classes first"}'
         if len(result)==0:
             db.insert({
                 "file":file.filename if hasattr(file,'filename') else None,
